374dc135bfb9139c88745d2d683f416d5905aeaa
saloniY001 <jamadarujef@gmail.com>
2024-11-20 15:39:28 +0530

    s

--- aws/task2/Utilities/dbConfig.js
+++ aws/task2/Utilities/dbConfig.js
@@ -1,4 +1,4 @@
-const {createPool,createConnection} = require('mysql')
+const { createPool, createConnection } = require('mysql')
 
 // const pool = createPool({
 //     port:process.env.DB_PORT,
@@ -9,25 +9,27 @@ const {createPool,createConnection} = require('mysql')
 //     connectionLimit:10
 // });
 
+
+
 const connection = createConnection({
-    port:process.env.DB_PORT,
-    host:process.env.DB_HOST,
-    user:process.env.DB_USER,
-    password:process.env.DB_PASSWORD,
-    database:process.env.DB_NAME,
+    port: process.env.DB_PORT,
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
     timezone: 'IST',
     multipleStatements: true,
     charset: 'UTF8MB4_GENERAL_CI',
-    connectionLimit:10
+    connectionLimit: 10
 });
 
 // pool.query('SELECT 1',(err,result)=>{
 //     if(err){
 //         console.log(err);
-        
+
 //     } else{
 //         console.log('db connected ',result);
-        
+
 //     }
 // })
 
